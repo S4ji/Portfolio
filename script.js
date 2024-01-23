c831a6be48c30e553dd917b92d21f7c35238f25a
Yass <edel.e.devweb@gmail.com>
2024-01-23 21:00:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener(
+    'scroll',
+    () => {
+        document.body.style.setProperty(
+            '--scroll',
+            window.pageYOffset /
+                (document.body.offsetHeight - window.innerHeight)
+        )
+    },
+    false
+)
